f44806da10bf6a34d859793c13b3504a1ae4cfda
ymgyt <yamaguchi7073xtt@gmail.com>
2019-04-05 18:23:29 +0900

    add prompt feature
    support prompting user input usecase.
    
    Issues
    - https://github.com/ymgyt/cli/issues/14

---
+++ prompt/prompt_test.go
@@ -0,0 +1,75 @@
+package prompt_test
+
+import (
+	"bytes"
+	"errors"
+	"io/ioutil"
+	"testing"
+	"time"
+
+	"github.com/ymgyt/cli/prompt"
+)
+
+func TestSession_Prompt(t *testing.T) {
+	t.Run("simple", func(t *testing.T) {
+		in := bytes.NewBufferString("yes\n")
+		out := &custom{buff: make([]byte, 0)}
+		msg := "are you sure?[yes/no]"
+		p := prompt.New().SetReader(in).SetWriter(out).Display(msg).Require("yes")
+		ok, err := p.Prompt()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !ok {
+			t.Fatal("want ok, but got not ok")
+		}
+		got, want := out.String(), msg
+		if got != want {
+			t.Errorf("displayed prompt message does not match. got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("case insensitive", func(t *testing.T) {
+		in := bytes.NewBufferString("YES\n")
+		msg := "type yes!"
+		p := prompt.New().SetReader(in).SetWriter(ioutil.Discard).Display(msg).Require("yes").CaseInsensitive()
+		ok, err := p.Prompt()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !ok {
+			t.Fatal("want ok, but got not ok")
+		}
+	})
+
+	t.Run("timeout", func(t *testing.T) {
+		in := &custom{}
+		p := prompt.New().SetReader(in).SetWriter(ioutil.Discard).Require("yes").Timeout(100 * time.Millisecond)
+		ok, err := p.Prompt()
+		if ok {
+			t.Fatal("want timeout error, but got ok")
+		}
+		if err != prompt.ErrTimeout {
+			t.Fatalf("want timeout error, but got %v", err)
+		}
+	})
+}
+
+type custom struct {
+	i    int
+	buff []byte
+}
+
+func (c *custom) Write(p []byte) (int, error) {
+	c.buff = append(c.buff, p[0])
+	c.i++
+	return 1, nil
+}
+
+func (c *custom) Read(p []byte) (int, error) {
+	select {}
+	return 0, errors.New("not sleeping") // nolint
+}
+
+func (c *custom) String() string { return string(c.buff) }
+
